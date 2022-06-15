077cb7cbd8ce0c5909223e36ef18dea3a791fc3e
qzj517 <qzj517@a00552.science.domain>
2022-06-15 13:59:05 +0200

    move testing scripts to gitignore

--- testing_ligands2.py
+++
@@ -1,5 +0,0 @@
-import equibind_inference 
-if __name__ == "__main__":
-    equibind_inference.main(["-l", "/home/qzj517/POR-DD/data/raw_data/cyp_screen/test_3D_opt_1216.sdf",
-    "-r", "/home/qzj517/POR-DD/data/raw_data/cyp_screen/protein.pdb", "-o", "/home/qzj517/POR-DD/data/equibind_processed/testing", 
-    "--n_workers_data_load", "0", "--batch_size", "32", "--no_skip", "--lazy_dataload"])
